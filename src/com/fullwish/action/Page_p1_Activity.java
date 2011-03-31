281dfaf12150cb9ac49de420862a1cd04afe1332
qx <qx.ouyang@gmail.com>
2011-03-31 17:06:53 +0800

    331

--- src/com/fullwish/action/Page_p1_Activity.java
+++ src/com/fullwish/action/Page_p1_Activity.java
@@ -5,115 +5,117 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
 public class Page_p1_Activity extends Activity {
-    private Button qiangdipan;
-
-    private TextView larentou;
+    private ImageButton qiangdipan;
+    private TextView p1_tv_14;
+    private ImageButton larentou;
 
     private ImageButton ji;
 
     private ImageButton quan;
 
-    private ImageButton  sha;
+    private ImageButton sha;
 
-    private Button zhuangbei;
+    private ImageButton zhuangbei;
 
-    private Button hanhua;
+    private ImageButton hanhua;
 
-    private Button haoyou;
+    private ImageButton haoyou;
 
-    private Button shengyi;
+    private ImageButton shengyi;
 
-    private Button dafu;
+    private ImageButton dafu;
 
-    private Button qianzhuang;
+    private ImageButton qianzhuang;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.p1);
-        qiangdipan = (Button) this.findViewById(R.id.rob);
-        larentou = (TextView) this.findViewById(R.id.invite);
-        ji = (ImageButton) this.findViewById(R.id.skills);
-        quan = (ImageButton) this.findViewById(R.id.kid);
-        sha = (ImageButton) this.findViewById(R.id.kill);
-        zhuangbei = (Button) this.findViewById(R.id.equi);
-        hanhua = (Button) this.findViewById(R.id.blog);
-        haoyou = (Button) this.findViewById(R.id.friend);
-        shengyi = (Button) this.findViewById(R.id.business);
-        dafu = (Button) this.findViewById(R.id.docter);
-        qianzhuang = (Button) this.findViewById(R.id.bank);
+        qiangdipan = (ImageButton) this.findViewById(R.id.p1_ib_03);
+        larentou = (ImageButton) this.findViewById(R.id.p1_ib_04);
+        ji = (ImageButton) this.findViewById(R.id.p1_ib_05);
+        quan = (ImageButton) this.findViewById(R.id.p1_ib_06);
+        sha = (ImageButton) this.findViewById(R.id.p1_ib_07);
+        zhuangbei = (ImageButton) this.findViewById(R.id.p1_ib_08);
+        hanhua = (ImageButton) this.findViewById(R.id.p1_ib_09);
+        haoyou = (ImageButton) this.findViewById(R.id.p1_ib_10);
+        shengyi = (ImageButton) this.findViewById(R.id.p1_ib_11);
+        dafu = (ImageButton) this.findViewById(R.id.p1_ib_12);
+        qianzhuang = (ImageButton) this.findViewById(R.id.p1_ib_13);
+        p1_tv_14= (TextView) this.findViewById(R.id.p1_tv_14);  
+        p1_tv_14.setSelected(true);  // Set focus to the textview
         OnClickListener clicklistener = new OnClickListener() {
             @Override
             public void onClick(View v) {
                 switch (v.getId()) {
-                case R.id.rob:/** Start a new Activity MyCards.java */
+                case R.id.p1_ib_03:/** Start a new Activity MyCards.java */
                     // Intent intent = new Intent(this, MyCards.class);
                     // this.startActivity(intent);
+                    
                     Intent intent31 = new Intent(Page_p1_Activity.this,
                             Page_p31_Activity.class);
                     startActivityForResult(intent31, 1);
                     System.out.println("click rob");
                     break;
-                case R.id.invite:/** AlerDialog when click on Exit */
+                case R.id.p1_ib_04:/** AlerDialog when click on Exit */
                     Intent intent3 = new Intent(Page_p1_Activity.this,
                             Page_p3_Activity.class);
                     startActivityForResult(intent3, 1);
                     System.out.println("click invite");
                     break;
-                case R.id.skills:/** AlerDialog when click on Exit */
+                case R.id.p1_ib_05:/** AlerDialog when click on Exit */
                     Intent intent5 = new Intent(Page_p1_Activity.this,
                             Page_p5_Activity.class);
                     startActivityForResult(intent5, 1);
                     System.out.println("skills");
                     break;
-                case R.id.kid:/** AlerDialog when click on Exit */
+                case R.id.p1_ib_06:/** AlerDialog when click on Exit */
                     Intent intent678 = new Intent(Page_p1_Activity.this,
                             Page_p678_Activity.class);
                     startActivityForResult(intent678, 1);
                     System.out.println("kid");
                     break;
-                case R.id.kill:/** AlerDialog when click on Exit */
+                case R.id.p1_ib_07:/** AlerDialog when click on Exit */
                     Intent intent12 = new Intent(Page_p1_Activity.this,
                             Page_p12_Activity.class);
                     startActivityForResult(intent12, 1);
                     System.out.println("kill");
                     break;
-                case R.id.equi:/** AlerDialog when click on Exit */
+                case R.id.p1_ib_08:/** AlerDialog when click on Exit */
                     Intent intent25 = new Intent(Page_p1_Activity.this,
                             Page_p25_Activity.class);
                     startActivityForResult(intent25, 1);
                     System.out.println("equi");
                     break;
-                case R.id.blog:/** AlerDialog when click on Exit */
+                case R.id.p1_ib_09:/** AlerDialog when click on Exit */
                     Intent intent14 = new Intent(Page_p1_Activity.this,
                             Page_p14_Activity.class);
                     startActivityForResult(intent14, 1);
                     System.out.println("blog");
                     break;
-                case R.id.friend:
+                case R.id.p1_ib_10:
                     Intent intent16 = new Intent(Page_p1_Activity.this,
                             Page_p16_Activity.class);
                     startActivityForResult(intent16, 1);
                     System.out.println("friend");
                     break;
-                case R.id.business:/** AlerDialog when click on Exit */
+                case R.id.p1_ib_11:/** AlerDialog when click on Exit */
                     Intent intent23 = new Intent(Page_p1_Activity.this,
                             Page_p23_Activity.class);
                     startActivityForResult(intent23, 1);
                     System.out.println("business");
                     break;
-                case R.id.docter:/** AlerDialog when click on Exit */
+                case R.id.p1_ib_12:/** AlerDialog when click on Exit */
                     Intent intent20 = new Intent(Page_p1_Activity.this,
                             Page_p20_Activity.class);
                     startActivityForResult(intent20, 1);
                     System.out.println("docter");
                     break;
-                case R.id.bank:/** AlerDialog when click on Exit */
+                case R.id.p1_ib_13:/** AlerDialog when click on Exit */
                     Intent intent24 = new Intent(Page_p1_Activity.this,
                             Page_p24_Activity.class);
                     startActivityForResult(intent24, 1);
